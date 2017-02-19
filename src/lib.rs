b31b68776ef6c6e30bcf52eebb16a00444b72212
Simon Harris <simon@pearfalse.com>
2017-02-19 16:38:14 +0000

    Minor fixes to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
-//! A create to parse [Acorn DFS](https://en.wikipedia.org/wiki/Disc_Filing_System) disc images. Currently, only in-memory reading
+//! A crate to parse [Acorn DFS](https://en.wikipedia.org/wiki/Disc_Filing_System) disc images. Currently, only in-memory reading
 //! of DFS discs is supported.
+
+#![crate_type = "lib"]
 
 extern crate core;
 
